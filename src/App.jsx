99c0904d6d6a6b8876989978a094ec62b3e8879e
Prerna <sprerna288@gmail.com>
2026-01-13 23:00:44 +0530

    media api added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { fetchPhotos } from './api/mediaApi'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen w-full bg-zinc-600'>
+      
+    </div>
+  )
+}
+
+export default App
